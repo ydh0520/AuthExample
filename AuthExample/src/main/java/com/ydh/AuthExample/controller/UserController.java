9a215f4a6a7be4208c9b6dcab6b21ea99595ce21
= <=>
2023-02-28 16:57:48 +0900

    Add UserController

--- AuthExample/src/main/java/com/ydh/AuthExample/controller/UserController.java
+++ AuthExample/src/main/java/com/ydh/AuthExample/controller/UserController.java
@@ -1,16 +1,43 @@
 package com.ydh.AuthExample.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ydh.AuthExample.model.entity.UserEntity;
 import com.ydh.AuthExample.model.response.BaseResponse;
+import com.ydh.AuthExample.service.UserService;
 
 @RestController
 @RequestMapping("/api")
 public class UserController {
+
+	@Autowired
+	UserService userService;
+
+	@GetMapping("/userlist")
+	public ResponseEntity<BaseResponse> userList() {
+		BaseResponse res = new BaseResponse();
+		
+		res.data = userService.usetList();
+		
+		return new ResponseEntity<>(res, HttpStatus.OK);
+	}
+
+	@PostMapping("/usersave")
+	public ResponseEntity<BaseResponse> userSave(@RequestBody UserEntity user) {
+		BaseResponse res = new BaseResponse();
+
+		res.data = userService.userSave(user);
+
+		return new ResponseEntity<>(res, HttpStatus.OK);
+	}
 
 	@GetMapping("/test")
 	public ResponseEntity<BaseResponse> test() {
